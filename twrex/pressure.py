a778c5e9e3379273401b6336db26f7ed37efd677
andywinhold <andrew.winhold@gmail.com>
2017-04-14 19:16:59 -0700

    temp work on pressure.py to update plot refs #3

--- twrex/pressure.py
+++ twrex/pressure.py
@@ -12,7 +12,7 @@ import sys
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-##import u3
+import u3
 
 def v_upconvert(V):
         """ Convert the incoming voltage back to voltage measured by pressure sensor.
@@ -81,95 +81,92 @@ if __name__ == '__main__':
         start_time = time.time()
 
         ###Empty lists for both pressure sensors
-        ##p1 = []
-        ##p2 = []
-        ##
+        p1 = []
+        p2 = []
+
         ### Establish connection to first U3 device found. We should only have one connected.
-        ##d = u3.U3()
-        ##
+        d = u3.U3()
+
         ### Get the calibration data of the device. The calibration data 
         ###will be used by functions that convert binary data to 
         ###voltage/temperature and vice versa.
-        ##d.getCalibrationData()
+        d.getCalibrationData()
         ##
         ### Check which FIOs are analog and which are digital
-        ##configDict = d.configIO()
-        ##configDict["FIOAnalog"]
-        ###63
-        ##
+        configDict = d.configIO()
+        configDict["FIOAnalog"]
+
         ### Set the first four (0-3) to analog (15 = 1111 binary) 
         ###and the rest to digital
-        ###d.configIO(FIOAnalog = 15)
-        ##
+        d.configIO(FIOAnalog = 15)
+
         ### Read from AIN0 in one function
-        ##ain0val = d.getAIN(0)
-        ##print "testing AIN0 {0:.5f}".format(ain0val)
+        ain0val = d.getAIN(0)
+        print "testing AIN0 {0:.5f}".format(ain0val)
         ###1.501376
-        ##
+
         ### Read from AIN1 in one function
-        ##ain1val = d.getAIN(1)
-        ##print "testing AIN1 {0:.5f}".format(ain1val)
-        ##print ain1val
+        ain1val = d.getAIN(1)
+        print "testing AIN1 {0:.5f}".format(ain1val)
+        print ain1val
         stuff = []
         xar = []
         yar = []
-        p1 = []
-        p2 = []
+
         filename = 'testing_keybord_inter1.csv'
-        fig = plt.figure()
-        ax = fig.add_subplot(111)
-        ax.set_xlabel('Time (s)')
-        ax.set_ylabel('Pressure (Torr)')
-        
+
+##        
         while (True):
                 # Get reading from first pressure sensor
-        ##      vval1 = d.getAIN(0)
-        ##	print "Uncalibrated reading for p1 {0:.5f}".format(vval1)
-        ##      calvval1 = v_upconvert(vval1)
-        ##      print "Calibrated reading for p1 {0:.5f}".format(calvval1)
+                vval1 = d.getAIN(0)
+                print "Uncalibrated voltage for p1:  {0:.5f}".format(vval1)
+                calvval1 = v_upconvert(vval1)
+                print "Calibrated voltage for p1:  {0:.5f}".format(calvval1)
 
                 # Get reading from second pressure sensor
-        ##	vval2 = d.getAIN(1)
-        ##	print "Uncalibrated reading for p2 {0:.5f}".format(vval2)
-        ##      calvval2 = v_upconvert(vval2)
-        ##      print "Calibrated reading for p2 {0:.5f}".format(calvval2)
+                vval2 = d.getAIN(2)
+                print "Uncalibrated voltage for p2:  {0:.5f}".format(vval2)
+                calvval2 = v_upconvert(vval2)
+                print "Calibrated voltage for p2:  {0:.5f}".format(calvval2)
                 
                 # Convert voltage to Torr
-        ##        pval1 = v2p(calvval1)
-        ##        pval2 = v2p(calvval2)
+                pval1 = v2p(calvval1)
+                pval2 = v2p(calvval2)
+                print "p1 pressure: {0:.15f}".format(pval1)
+                print "p2 pressure: {0:.15f}".format(pval2)
 
-                time.sleep(2)
-                p1int = raw_input('add1')
-                p2int = raw_input('add2')
-                if p1int == 'done':
-                        print "end of collection"
-                        print stuff
-                        break
-                p1fl = float(p1int)
-                p2fl = float(p2int)
-                p1.append(p1fl)
-                p2.append(p2fl)
+                time.sleep(5)
+##                p1int = raw_input('add1')
+##                p2int = raw_input('add2')
+##                if p1int == 'done':
+##                        print "end of collection"
+##                        print stuff
+##                        break
+##                p1fl = float(p1int)
+##                p2fl = float(p2int)
+##                p1.append(p1fl)
+##                p2.append(p2fl)
 ##                stuff.append(add)
 
                 # add readings to their lists
-        ##        pval1 = float(pval1)
-        ##        pval2 = float(pval2)
-        ##        p1.append(pval1)
-        ##        p2.append(pval2)
+                pval1 = float(pval1)
+                pval2 = float(pval2)
+                p1.append(pval1)
+                p2.append(pval2)
                 print("--- Time elapsed: %s seconds ---" % (time.time() - start_time))
-##                # make lists into data frame
-                df = pd.DataFrame({'Sensor 1': p1, 'Sensor 2': p2})
-##                df.drop(['Unnamed: 0'])
-
-##                # save to .csv file then read
-                df.to_csv(filename, sep=',')
-                #new_df = pd.read_csv(filename)
-                #This row may be needed
-                
-
-                animation.FuncAnimation(fig,animate, interval=10)
-                plt.show()
-                # create plot
+####                # make lists into data frame
+##                df = pd.DataFrame({'Sensor 1': p1, 'Sensor 2': p2})
+####                df.drop(['Unnamed: 0'])
+##
+####                # save to .csv file then read
+##                df.to_csv(filename, sep=',')
+##                new_df = pd.read_csv(filename)
+##                #This row may be needed
+##                #del df['Unamed: 0']
+##
+##                #animation.FuncAnimation(fig,animate, interval=10)
+##                #plt.show()
+##                # create plot
 ##                ax = new_df.plot.area(stacked=False)
 ##                ax.set_xlabel('Time (s)')
 ##                ax.set_ylabel('Pressure (Torr)')
@@ -177,25 +174,25 @@ if __name__ == '__main__':
 
     except KeyboardInterrupt:
             # make lists into data frame
-##            df = pd.DataFrame({'Sensor 1': p1, 'Sensor 2': p2})
+            df = pd.DataFrame({'Sensor 1': p1, 'Sensor 2': p2})
 
             # save to .csv file
-##            df.to_csv(filename, sep=',')
-            sys.exit(0)
-##            # make lists into data frame
-##            df = pd.DataFrame({'Sensor 1': p1, 'Sensor 2': p2})
+            df.to_csv(filename, sep=',')
+            #sys.exit(0)
+            # make lists into data frame
+            df = pd.DataFrame({'Sensor 1': p1, 'Sensor 2': p2})
 ##
-##            # save to .csv file then read
-##            df.to_csv(filename, sep=',')
-##            new_df = pd.read_csv(filename)
-##            #This row may be needed
-##            del new_df['Unnamed: 0']
-##
-##            # create plot
-##            ax = new_df.plot.area(stacked=False)
-##            ax.set_xlabel('Time (s)')
-##            ax.set_ylabel('Pressure (Torr)')
-##            plt.show()
+            # save to .csv file then read
+            df.to_csv(filename, sep=',')
+            new_df = pd.read_csv(filename)
+            #This row may be needed
+            del new_df['Unnamed: 0']
+
+            # create plot
+            ax = new_df.plot.area(stacked=False)
+            ax.set_xlabel('Time')
+            ax.set_ylabel('Pressure (Torr)')
+            plt.show()
 
             #df1 = pd.DataFrame(stuff)
             ##df1.to_csv('test1.csv', sep=',')
